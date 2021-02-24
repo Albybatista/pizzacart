7a78b27d030b5529c34081344c45b8e1d06aee69
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-02-24 00:32:38 -0500

    sidebar working

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../Navbar';
 import { HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn } from './HeroStyles';
+import Sidebar from '../Sidebar';
 
 const Hero = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
         <HeroContainer>
-            <Navbar />
+            <Navbar toggle={toggle}/>
+            <Sidebar isOpen={isOpen} toggle={toggle} />
             <HeroContent>
                 <HeroItems>
                     <HeroH1>Best Pizza at Your Door</HeroH1>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import './Navbar.css'
 import { Nav, NavLink, NavIcon, MenuIcon } from './NavbarStyles';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
     return (
         <>
             <Nav>
                 <NavLink to='/'>Pizza</NavLink>
-                <NavIcon>
+                <NavIcon onClick={ toggle }>
                     <p>Menu</p>
                     <MenuIcon />
                 </NavIcon>
