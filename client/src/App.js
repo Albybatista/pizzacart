0efc646a286e6406333866d128a3e1da06f86068
Alby Batista <67451368+Albybatista@users.noreply.github.com>
2021-02-27 15:10:51 -0500

    added products folder and removed server folder

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { GlobalStyle } from './globalStyles';
 import Hero from './components/Hero';
-
+import { productData } from './components/Products/data'
 
 function App() {
   return (
     <Router>
       <GlobalStyle />
       <Hero />
+      <Products heading='Choose your Pizza' data={productData} />
     </Router>
   );
 }
